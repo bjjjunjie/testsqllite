9dd0f24553c8b116bed66eac80afce8a99b26e1c
baijunjie <bjjjunjie@126.com>
2022-11-17 10:42:40 +0800

    测试添加数据

--- testsqlite/testsqlite/Pages/Index.cshtml.cs
+++ testsqlite/testsqlite/Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using testsqlite.DataAccess;
+using testsqlite.Models;
 
 namespace testsqlite.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private MyDbContext _myDbContext;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,MyDbContext myDbContext)
         {
             _logger = logger;
+            _myDbContext = myDbContext;
         }
 
         public void OnGet()
         {
-
+            User user = new User() {Name="222"};
+            _myDbContext.User.Add(user);
+            _myDbContext.SaveChanges();
         }
     }
 }
